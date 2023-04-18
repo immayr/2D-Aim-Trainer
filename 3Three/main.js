bed69573181b389f66123a0216932be6915d98c9
immayr <87451439+immayr@users.noreply.github.com>
2023-04-18 18:03:57 -0400

    Update main.js

--- 3Three/main.js
+++ 3Three/main.js
@@ -1,20 +1,44 @@
-    const htmlEditor = document.getElementById("html-editor");
-    const cssEditor = document.getElementById("css-editor");
-    const jsEditor = document.getElementById("js-editor");
-    const output = document.getElementById("output");
+// JavaScript for the editors
+var htmlEditor = ace.edit("html-editor");
+htmlEditor.setTheme("ace/theme/monokai");
+htmlEditor.session.setMode("ace/mode/html");
+htmlEditor.setOptions({
+  enableBasicAutocompletion: true,
+  enableLiveAutocompletion: true,
+  enableSnippets: true
+});
 
-    const updateOutput = () => {
-      const html = htmlEditor.value;
-      const css = `<style>${cssEditor.value}</style>`;
-      const js = `<script>${jsEditor.value}</script>`;
+var cssEditor = ace.edit("css-editor");
+cssEditor.setTheme("ace/theme/monokai");
+cssEditor.session.setMode("ace/mode/css");
+cssEditor.setOptions({
+  enableBasicAutocompletion: true,
+  enableLiveAutocompletion: true,
+  enableSnippets: true
+});
 
-      const combinedCode = html + css + js;
+var jsEditor = ace.edit("js-editor");
+jsEditor.setTheme("ace/theme/monokai");
+jsEditor.session.setMode("ace/mode/javascript");
+jsEditor.setOptions({
+  enableBasicAutocompletion: true,
+  enableLiveAutocompletion: true,
+  enableSnippets: true
+});
 
-      output.contentWindow.document.open();
-      output.contentWindow.document.write(combinedCode);
-      output.contentWindow.document.close();
-    };
+// JavaScript for the Run button
+var runBtn = document.getElementById("run-btn");
+runBtn.addEventListener("click", function() {
+  var output = document.getElementById("output");
+  output.innerHTML = '<style>' + cssEditor.getValue() + '</style>' + htmlEditor.getValue() + '<script>' + jsEditor.getValue() + '</script>';
+});
 
-    htmlEditor.addEventListener("input", updateOutput);
-    cssEditor.addEventListener("input", updateOutput);
-    jsEditor.addEventListener("input", updateOutput);
+// JavaScript for the Reset button
+var resetBtn = document.getElementById("reset-btn");
+resetBtn.addEventListener("click", function() {
+  htmlEditor.setValue("");
+  cssEditor.setValue("");
+  jsEditor.setValue("");
+  var output = document.getElementById("output");
+  output.innerHTML = "";
+});
